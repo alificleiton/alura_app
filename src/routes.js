20f00c19507571f0644ba67362ba21ca72330ebb
unknown <alificleiton123@hotmail.com>
2022-11-22 16:12:09 -0300

    Configurando rotas com ROuter DOM

---
+++ src/routes.js
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Routes, Route }  from 'react-router-dom';
+import Cardapio from 'pages/Cardapio';
+import Inicio from 'pages/Inicio';
+import Menu from 'components/Menu';
+import PaginaPadrao from 'components/PaginaPadrao';
+import Sobre from 'pages/Sobre';
+
+
+export default function AppRouter() {
+    return (
+        <main>
+            <Router>
+                <Menu />
+                <Routes>
+                    <Route path='/' element={<PaginaPadrao/>} >
+                        <Route index element={<Inicio />} />
+                        <Route path='cardapio' element={<Cardapio />}/>
+                        <Route path='sobre' element={<Sobre />} /> 
+                    </Route>
+                </Routes>
+            </Router>
+        </main>
+    );
+}
